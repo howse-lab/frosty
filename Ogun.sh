3358951da25f7d850437f66c734bc2a9457f0f41
SnoopyPuppy865 <femio765@gmail.com>
2022-02-24 14:02:02 -0700

    Added wifes of Ogun Governor

---
+++ Ogun.sh
@@ -0,0 +1,14 @@
+Ise ya Ise ya 
+Omo Ogun Ise Ya
+Ise Ma Ya ooooo
+
+Ise ya Ise ya
+Omo Ogun Ise ya
+Eludumare Ise Ma Yo oooo
+EHhh
+
+Adedapo Oluseun Abiodun
+Bamidele Abiodun
+
+Ibikunle Amosun
+Olufunso Amosun
